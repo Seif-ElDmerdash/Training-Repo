0cb706e544e3d078ea73f232e1620740b4fbd084
Seif El Dmerdash <S.Mohamed2533@nu.edu.eg>
2026-01-27 21:06:18 +0200

    Watermelon problem

--- Training.cpp
+++ Training.cpp
@@ -1,26 +1,46 @@
+// #include <iostream>
+// #include <cmath>
+
+// int main(){
+//     using namespace std;
+
+//     double a; double b; double c;
+
+//     cout << "Enter A:";
+//     cin >> a;
+//     cout << "Enter B:";
+//     cin >> b;
+//     cout << "Enter C:";
+//     cin >> c;
+
+//     if(a==b && b==c && a==c){
+//         cout << "This is an Equilatoral Triangle!";
+//     }
+//     else if (a==b || b==c || a==c){ 
+//         cout << "This is an Isoceles Triangle!";
+//     }
+//     else{
+//         cout << "This a Scalne Triangle!";
+//     }
+
+//     return 0;
+// }
+
+// Watermelon problem ;P
+
 #include <iostream>
-#include <cmath>
 
 int main(){
-    using namespace std;
+    using std::cout; using std::cin; 
 
-    double a; double b; double c;
-
-    cout << "Enter A:";
-    cin >> a;
-    cout << "Enter B:";
-    cin >> b;
-    cout << "Enter C:";
-    cin >> c;
-
-    if(a==b && b==c && a==c){
-        cout << "This is an Equilatoral Triangle!";
-    }
-    else if (a==b || b==c || a==c){ 
-        cout << "This is an Isoceles Triangle!";
+    int w;
+    cout << "Enter a number from 1 - 100: ";
+    cin >> w;
+    if(w%2 == 0){
+        cout << "YES";
     }
     else{
-        cout << "This a Scalne Triangle!";
+        cout << "NO";
     }
 
     return 0;
