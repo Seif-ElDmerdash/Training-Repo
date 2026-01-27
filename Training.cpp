12867091bfb3d0363f0803af8f38dfd29aaf7fec
Seif El Dmerdash <S.Mohamed2533@nu.edu.eg>
2026-01-27 21:48:42 +0200

    Simple calculator

--- Training.cpp
+++ Training.cpp
@@ -28,19 +28,67 @@
 
 // Watermelon problem ;P
 
+// #include <iostream>
+
+// int main(){
+//     using std::cout; using std::cin; 
+
+//     int w;
+//     cin >> w;
+//     if(1<=w<=100)
+//         {if(w%2 == 0 && w!=2){
+//             cout << "YES";
+//         }
+//         else{
+//             cout << "NO";
+//         }}
+//     return 0;
+// }
+
+// Simple Caculator
+
 #include <iostream>
 
 int main(){
-    using std::cout; using std::cin; 
 
-    int w;
-    cin >> w;
-    if(1<=w<=100)
-        {if(w%2 == 0 && w!=2){
-            cout << "YES";
-        }
-        else{
-            cout << "NO";
-        }}
+    using std::cout; using std::cin;
+
+    double num1; double num2;double result;
+    char op;
+
+    cout << "************ CALCULATOR ************" << "\n";
+
+    cout << "Enter operation (+ - / *)" << "\n";
+    cin >> op;
+    cout << "Enter #1: ";
+    cin >> num1;
+    cout << "Enter #2: ";
+    cin >> num2;
+    switch(op){
+        case '+':
+            result = num1 + num2;
+            cout << result << "\n";
+            break;
+        case '-':
+            result = num1 - num2;
+            cout << result << "\n";
+            break;
+        case '*':
+            result = num1 * num2;
+            cout << result << "\n";
+            break;
+        case '/':
+            result = num1 / num2;
+            cout << result << "\n";
+            break;
+        default:
+            cout << "Invalid response" << "\n";
+    }
+
+
+    cout << "************************************";
+
+
+
     return 0;
 }
