c81057c2a16fcea0dbd5805a7bea06ae5bffda3b
Seif El Dmerdash <S.Mohamed2533@nu.edu.eg>
2026-02-25 13:15:36 +0200

    LAB 1

---
+++ lab1.cpp
@@ -0,0 +1,78 @@
+// #include <iostream>
+
+// int main(){
+
+//     using namespace std;
+
+//     int x,y;
+//     cout<<"Enter 2 numbers:\n"; cin >> x >> y;
+
+//     cout<< x*y;
+
+//     return 0;
+// }
+
+
+// #include <iostream>
+
+// int main(){
+
+//     using namespace std;
+
+//     string name1,name2;
+
+//     cout<<"Enter your First and Last name: \n"; cin>>name1>>name2;
+
+//     cout<< name1<<" "<<name2;
+
+//     return 0;
+// }
+
+
+
+// #include <iostream>
+
+// int main(){
+//     using namespace std;
+
+//     int age;
+
+//     cout<<"Enter your age:\n"; cin>>age;
+    
+//     cout<<((18<=age && age<60)? "You are allowed to drive" : "Not allowed");
+
+//     return 0;
+// }
+
+// #include <iostream>
+
+// int main(){
+//     using namespace std;
+
+//     int num1; float num2;
+
+//     cin>>num1>>num2;
+
+//     cout<< "as int: " <<int (num1+num2)<<endl;
+//     cout << "as float: "<< float(num1+num2);
+
+//     return 0;
+// }
+
+
+// #include <iostream>
+
+// int main(){
+//     using namespace std;
+
+
+//     double y;
+//     cout<<"Enter number for Y: \n";cin>>y;
+
+//     cout<< (y +=5)<<endl;
+//     cout<< (y -=2)<<endl;
+//     cout<< (y *=3)<<endl;
+//     cout<< (y /=2);
+
+//     return 0;
+// }
